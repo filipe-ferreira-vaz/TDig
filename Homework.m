ec98fe746b7028f528a9216038e7963a16fbb671
Filipe Vaz <filipe14vaz@gmail.com>
2024-12-01 15:06:16 +0000

    2.15

--- Homework.m
+++ Homework.m
@@ -46,3 +46,20 @@ simb_prob_min = simb(idx_min); % Símbolos menos prováveis
 prob_simb_max = max(prob_simb);
 idx_max = find(prob_simb == prob_simb_max); % Índices de todos os símbolos mais prováveis
 simb_prob_max = simb(idx_max); % Símbolos mais prováveis
+
+%%% Exercício 2.11 %%%
+
+[prob_simb_no_zero, sort_idx] = sort(prob_simb);
+simb_no_zero = simb(sort_idx);
+
+%%% Exercício 2.12 %%%
+
+Entropia = -sum(prob_simb_no_zero .* log2(prob_simb_no_zero));
+Conteudo_Decisao = log2(size(simb,1));
+Redundancia = Conteudo_Decisao - Entropia;
+
+%%% Exercício 2.15 %%%
+
+R = fs*Entropia;
+Rd = fs*Conteudo_Decisao;
+Db_Redund = fs*Redundancia;
